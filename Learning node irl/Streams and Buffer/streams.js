a07de84b528a9ab5877874f6bcab9ada2605d8c6
Barkot Solomon <barkotsolomon5@gmail.com>
2026-08-18 15:22:42 +0300

    More about streams

--- Learning node irl/Streams and Buffer/streams.js
+++ Learning node irl/Streams and Buffer/streams.js
@@ -1,8 +1,12 @@
 const fs = require('fs');
 
 const readStream = fs.createReadStream('./docs/text.txt', { encoding:'utf8' });
+const writeStream = fs.createWriteStream('./docs/result.txt');
 
 readStream.on('data', (chunk => {
-    console.log('----- NEW CHUNK -----');
+    console.log('\n|----- NEW CHUNK -----|\n');
     console.log(chunk);
+    writeStream.write('\nNEW CHUNK\n');
+    writeStream.write(chunk);
 }))
+
